a24738d85c67716e7e3d090b240e809138cc72eb
Benjamin Rau <benngiro@gmail.com>
2015-02-04 20:53:14 -0500

    Update test5a.c
    Lock the mutex (otherwise we end up unlocking a unlocked mutex).

--- tests-pr01/more_tests/test5a.c
+++ tests-pr01/more_tests/test5a.c
@@ -30,6 +30,7 @@ int main()
 	gtthread_init(1000);
 
 	gtthread_mutex_init(&g_mutex);
+        gtthread_mutex_lock(&g_mutex);
 	gtthread_create(&th1, worker, NULL);
 	gtthread_create(&th2,worker,NULL);
 	printf("Main is printing something\n");
